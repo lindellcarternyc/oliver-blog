c75c67c1ec7f0b0c4293ff0fa98c4ee4d8bcc5ca
Lindell Carter <lindellcarternyc@gmail.com>
2018-10-09 17:03:23 -0400

    update layout colors

--- src/components/LayoutRoot.tsx
+++ src/components/LayoutRoot.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react'
 import styled from 'react-emotion'
 
+import { colors } from '../styles/variables'
+
 const StyledLayoutRoot = styled.div`
   display: flex;
   flex-direction: column;
   min-height: 100vh;
+  background-color: ${colors.black};
+  color: ${colors.white};
 `
 
 interface LayoutRootProps {
